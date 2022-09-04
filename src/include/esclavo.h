c6cebfc238dbecac2d989d94303c6826fe2e4b84
laucha12 <lautihernando@gmail.com>
2022-09-04 12:55:02 -0300

    esclavo calcula md5

---
+++ src/include/esclavo.h
@@ -0,0 +1,21 @@
+#ifndef esclavo_H
+#define esclavo_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <string.h>
+#define MAXBUFFER 250
+
+/*
+    Params: buffer en donde se pondra el valor del md5 con el nombre de aplicacion
+    void md5Calculate(char * buffer);
+*/
+void md5Calculate(char * buffer);
+
+
+
+
+
+#endif
