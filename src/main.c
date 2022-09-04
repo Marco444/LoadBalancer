c3044621b110f456b50ddfc8781d22decb4dd6f8
martinippo01 <martinippo@gmail.com>
2022-09-04 12:19:04 -0300

    File checking and access

--- src/main.c
+++ src/main.c
@@ -1,3 +1,11 @@
+#include<stdio.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+
+
 //
 // Created by Marco Scilipoti on 04/09/2022.
 //
@@ -6,3 +14,20 @@
 //
 // Created by Ippo on 04/09/2022.
 //
+
+int main(int argc, char * argv[]){
+
+    if(argc < 2){
+        printf("Usage: ./md5 <file1> <file2> ... <fileN>\n");
+    }
+
+    struct stat stats;
+
+    for(int i = 1; i < argc; i++){
+        if(stat(argv[i], &stats) != 0){
+            printf("Error! : cannot access '%s' file\n", argv[i]);
+            // Now can deliver files to slaves
+        }
+    }
+
+}
