23cad52a21a4672f4e601724fffb9e0daea51995
laucha12 <lautihernando@gmail.com>
2022-09-10 15:51:38 -0300

    pu;ll

--- src/testing/testLoadDispatcher.c
+++
@@ -1,9 +0,0 @@
-// This is a personal academic project. Dear PVS-Studio, please check it.
-// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
-//
-// Created by Marco Scilipoti on 05/09/2022.
-//
-
-int main() {
-
-}
